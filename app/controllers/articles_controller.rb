42746a82de32a8368ed03aa1c4747b74d5929266
dadaismu5 <carlo5@woorx.mx>
2015-12-25 21:22:24 -0600

    Nueva actualización
    Ahora tiene la opción de eliminar post.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,17 @@
 class ArticlesController < ApplicationController
-
 	def index
+		#Nos busca todos los registros en la BD
 		@articles = Article.all
 	end
 	#GET /articles/:id
 	def show
+		#Busca los registros por ID
 		@article = Article.find(params[:id])
+		#Where
 	end
 	#GET /articles/new
 	def new 
+		# crea nuevo registro
 		@article = Article.new
 	end
 	#POST /articles
@@ -18,10 +21,15 @@ class ArticlesController < ApplicationController
 				redirect_to @article
 		else
 			render :new
-		end
-			
+		end	
+	end
+		def destroy
+			@article= Article.find(params[:id])
+			@article.destroy #Destroy elimina el objeto de la base de datos
+			redirect_to articles_path
 	end
 	#PUT /articles/:id
 	def update
+		# @article.update_atributes({title: 'nuevo titulo'})
 	end
 end
